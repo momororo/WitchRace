606133a2625b9a858aa7d1cc7749b27740d9df3e
新井脩司 <araishuuji@araishuuji-no-MacBook-Pro.local>
2015-01-21 14:12:46 +0900

    コミット

--- Classes/TutorialScene.cpp
+++ Classes/TutorialScene.cpp
@@ -10,6 +10,8 @@
 #include "TitleScene.h"
 #include <string.h>
 
+cocos2d::Label *tutorial_text;
+
 //画面サイズの取得および設定
 #define selfFrame Director::getInstance() -> getWinSize()
 
@@ -36,11 +38,59 @@ bool TutorialScene::init(){
         return false;
     }
     
+    //背景作成
     auto tutorialBg = Sprite::create("tutorialBg.png");
     tutorialBg -> setPosition(Vec2(selfFrame.width/2,selfFrame.height/2));
     this -> addChild(tutorialBg);
     
+    //キキ作成
+    auto tutorial_kiki = Sprite::create("kiki_up.png");
+    tutorial_kiki -> setPosition(Vec2(selfFrame.width/3,selfFrame.height/3));
+    tutorial_kiki -> setName("tutorial_kiki");
+    tutorial_kiki -> setVisible(false);
+    this -> addChild(tutorial_kiki);
+    
+    //指作成
+    auto tutorial_hand = Sprite::create("tutorial_hand.png");
+    tutorial_hand -> setPosition(Vec2(selfFrame.width*2/3,selfFrame.height/2));
+    tutorial_hand -> setName("tutorial_hand");
+    tutorial_hand -> setVisible(false);
+    this -> addChild(tutorial_hand);
+    
+    //リング作成
+    auto tutorial_tapHold = Sprite::create("tutorial_tapHold.png");
+    tutorial_tapHold -> setPosition(Vec2(selfFrame.width*0.64,selfFrame.height*0.56));
+    tutorial_tapHold -> setName("tutorial_tapHold");
+    tutorial_tapHold -> setVisible(false);
+    this -> addChild(tutorial_tapHold);
+    
+    //タップ時の効果リング作成
+    auto tutorial_tapHoldEffect = Sprite::create("tutorial_tapHold.png");
+    tutorial_tapHoldEffect -> setPosition(Vec2(tutorial_tapHold->getPosition().x,tutorial_tapHold->getPosition().y));
+    tutorial_tapHoldEffect -> setName("tutorial_tapHoldEffect");
+    tutorial_tapHoldEffect -> setVisible(false);
+    this -> addChild(tutorial_tapHoldEffect);
+    
+    
+    //説明文作成
+    tutorial_text = Label::createWithSystemFont("激ムズの糸と同じ操作性です。", "HiraKakuPro-W3", 25);
+    tutorial_text -> setTextColor(Color4B::BLACK);
+    tutorial_text -> setPosition(Vec2(selfFrame.width/2,selfFrame.height/4));
+    tutorial_text -> setName("tutorial_text");
+    this -> addChild(tutorial_text);
+    
+    //戻るボタン作成
     setBackBt();
+    
+    //最初のフェーズの準備
+    setPhase();
+    
+    
+    
+    
+    
+    
+    
         
     /**************　タッチイベント設定  ******************/
     
@@ -140,4 +190,83 @@ void TutorialScene::setBackBt(){
     startMenu->setPosition(Vec2(selfFrame.width/2,100 + backBt->getContentSize().height/2));
     this->addChild(startMenu);
     
+}
+
+//フェーズ進行の準備
+void TutorialScene::setPhase(){
+    
+    //キキ、指、最初の説明文の表示
+    this -> getChildByName("tutorial_kiki") -> setVisible(true);
+    this -> getChildByName("tutorial_hand") -> setVisible(true);
+    this -> scheduleOnce(schedule_selector(TutorialScene::setPhase1), 5);
+    
+}
+
+//フェーズ１(上昇の説明)
+void TutorialScene::setPhase1(float time){
+    
+    //タップ時のリングとエフェクトを表示
+    this -> getChildByName("tutorial_tapHold")->setVisible(true);
+    this -> getChildByName("tutorial_tapHoldEffect")->setVisible(true);
+    
+    
+    //キキの上昇
+    this -> getChildByName("tutorial_kiki")->runAction(MoveTo::create(3, Vec2(this->getChildByName("tutorial_kiki")->getPosition().x,selfFrame.height*2/3)));
+    
+    
+    //エフェクトリングに効果をつける
+    auto scale = ScaleTo::create(1, 1.2);
+    auto fadeTo = FadeTo::create(1, 0);
+    auto scaleFade = Spawn::create(scale,fadeTo, NULL);
+    
+    auto reScale = ScaleTo::create(0, 1.0);
+    auto reFadeTo = FadeTo::create(0, 250);
+    auto reScaleFade = Spawn::create(reScale,reFadeTo, NULL);
+    
+    auto seq = Sequence::create(scaleFade,reScaleFade, NULL);
+    
+    auto repeat = RepeatForever::create(seq);
+    
+    this -> getChildByName("tutorial_tapHoldEffect")->runAction(repeat);
+    
+    
+    //説明文の変更
+    LanguageType language = Application::getInstance()->getCurrentLanguage();
+    if (language == LanguageType::JAPANESE) {
+        tutorial_text -> setString("画面をタップし続けると上昇します。");
+    }else{
+        tutorial_text -> setString("When a tap keeps doing a screen,she rises.");
+    }
+    
+    //5秒後にフェーズ２に移行
+    this->scheduleOnce(schedule_selector(TutorialScene::setPhase2), 5);
+
+}
+
+//フェーズ２(下降の説明)
+void TutorialScene::setPhase2(float time){
+    
+    //タップ時のリングとエフェクトを非表示に
+    this -> getChildByName("tutorial_tapHold")->setVisible(false);
+    this -> getChildByName("tutorial_tapHoldEffect")->setVisible(false);
+
+    
+    //キキの下降
+    this -> getChildByName("tutorial_kiki")->runAction(MoveTo::create(3, Vec2(this->getChildByName("tutorial_kiki")->getPosition().x,selfFrame.height*1/3)));
+    
+    
+    //説明文の変更(多言語化)
+    LanguageType language = Application::getInstance()->getCurrentLanguage();
+    if(language == LanguageType::JAPANESE){
+        tutorial_text -> setString("指を離すと下降します。");
+    }else{
+        tutorial_text -> setString("When a finger is separated from a screen,\nshe descends.");
+    }
+    
+    
+    //フェーズ１に移行
+    this -> scheduleOnce(schedule_selector(TutorialScene::setPhase1), 5);
+    
+    /***** ここからはフェーズ１→フェーズ２→フェーズ１･･･、の繰り返しになります。******/
+
 }
