7735c1d619b6e939ea20cc3ec8a3faf2455864db
新井脩司 <araishuuji@araishuuji-no-MacBook-Pro.local>
2015-02-08 16:03:54 +0900

    購入実装

--- Classes/SelectCharacterScene.cpp
+++ Classes/SelectCharacterScene.cpp
@@ -38,26 +38,6 @@ bool SelectCharacterScene::init()
         return false;
     }
     
-//説明文の枠作成
-    //キャラクターの名前
-    charaNameText = Label::createWithSystemFont("", "HiraKakuPro-W3", 50);
-    charaNameText -> setPosition(Vec2(selfFrame.width/2,selfFrame.height/3));
-    charaNameText -> setColor(Color3B::BLACK);
-    this -> addChild(charaNameText);
-    
-    //キャラクターの説明
-    charaExText = Label::createWithSystemFont("", "HiraKakuPro-W3", 25);
-    charaExText -> setPosition(Vec2(selfFrame.width/2,charaNameText->getPosition().y+charaExText->getContentSize().height));
-    charaExText -> setColor(Color3B::BLACK);
-    this -> addChild(charaExText);
-    
-    //キャラクター解放前の解放条件説明
-    charaLockText = Label::createWithSystemFont("", "HiraKakuPro-W3", 25);
-    charaLockText -> setVisible(false);
-    charaLockText -> setPosition(Vec2(selfFrame.width/2,selfFrame.height/3));
-    charaLockText -> setColor(Color3B::BLACK);
-    this -> addChild(charaLockText);
-    
 //型枠の作成
     auto bg = Sprite::create("character_Bg.png");
     
@@ -65,14 +45,42 @@ bool SelectCharacterScene::init()
     bg->setPosition(Vec2(selfFrame.width/2,selfFrame.height/2));
     this->addChild(bg);
     
+//タイトル・獲得ポイント表示
+    //タイトル作成
+    //auto titleLabel = Label::createWithSystemFont("select character", "MagicSchoolOne", 120);
+    auto titleLabel = Label::createWithSystemFont("select character", "MagicSchoolOne", 120);
+    titleLabel -> setColor(Color3B::BLACK);
+    titleLabel -> setPosition(Vec2(selfFrame.width/2,selfFrame.height*10/11));
+    this -> addChild(titleLabel,10);
+    
+    //userDefから獲得ポイント呼び出し準備
+    auto userDef = UserDefault::getInstance();
+
+    std::string totalStr = StringUtils::format("point : %d",userDef -> getIntegerForKey("playPoint"));
+    
+    //ポイントラベル作成
+    auto pointLabel = Label::createWithSystemFont(totalStr, "MagicSchoolOne", 60);
+    pointLabel -> setColor(Color3B::BLACK);
+    pointLabel -> setName("pointLabel");
+    pointLabel -> setPosition(Vec2(selfFrame.width/2,titleLabel->getPosition().y-pointLabel->getContentSize().height*9/10));
+    this -> addChild(pointLabel,10);
+    
+//説明文の枠作成
+    
+    //対象の説明テキスト用意
+    charaExLabel = Label::createWithSystemFont("", "HiraKakuPro-W3", 25);
+    charaExLabel -> setAlignment(TextHAlignment::CENTER);
+    charaExLabel -> setPosition(Vec2(selfFrame.width/2,selfFrame.height/3));
+    charaExLabel -> setColor(Color3B::BLACK);
+    this -> addChild(charaExLabel);
+    
+    
 //ボタンの作成(for文で回す？？)
 
     //ユーザーデフォルトを呼び出してキャラクターフラグを確認
-    auto userDef = UserDefault::getInstance();
-    //デバグ
-    userDef->setBoolForKey("characterFlag1", true);
-    userDef->setBoolForKey("characterFlag2", true);
-    userDef->setBoolForKey("characterFlag3", true);
+    userDef->setBoolForKey("characterFlag1", false);
+    userDef->setBoolForKey("characterFlag2", false);
+    userDef->setBoolForKey("characterFlag3", false);
     userDef->setBoolForKey("characterFlag4", false);
     userDef->setBoolForKey("characterFlag5", false);
 
@@ -83,6 +91,7 @@ bool SelectCharacterScene::init()
         
         auto characterFlag = userDef->getBoolForKey(StringUtils::format("characterFlag%d",idx).c_str());
 
+
         //スプライトの呼び出し
         Sprite *character;
 
@@ -90,61 +99,50 @@ bool SelectCharacterScene::init()
         if(characterFlag == true){
             //キャラクターの画像に
             switch (idx) {
-                case 0:
-                    character = Sprite::create("character_kiki.png");
-                    //charaNameText -> setString("見習い魔女");
-                    //charaExText -> setString("飛ぶのが得意なおてんば少女\n夢に向かって修行中");
-                    break;
-                
-                case 1:
-                    character = Sprite::create("character_petitKiki.png");
-                    //charaNameText -> setString("ひよっこ魔女");
-                    //charaExText -> setString("飛ぶのが苦手なあなたに、まずは練習用\n〜キャラクターボーナス〜\n障害物にあたりにくくなる");
-                    break;
-                
-                case 2:
-                    character = Sprite::create("character_harryPotter.png");
-                    //charaNameText -> setString("イナズマボーイ");
-                    //charaExText -> setString("僕は生き残る！\n〜キャラクターボーナス〜\nストーリクリアポイントに10ポイント加算");
-                    break;
-                
-                case 3:
-                    character = Sprite::create("character_ponyo.png");
-                    //charaNameText -> setString("空を駆ける女の子");
-                    //charaExText -> setString("元気いっぱい！空もかけちゃお♪\n〜キャラクターボーナス〜\nストーリクリアポイント✕3のポイント加算");
-                    break;
-                
-                case 4:
-                    character = Sprite::create("character_fordAnglia.png");
-                    //charaNameText -> setString("空飛ぶ車");
-                    //charaExText -> setString("オジサンからかりた車\n〜キャラクターボーナス〜\nストーリークリアポイント✕5のポイント加算");
-                    break;
-                
-                case 5:
-                    character = Sprite::create("character_porcoRosso.png");
-                    //charaNameText -> setString("元エースパイロット");
-                    //charaExText -> setString("自由気ままに生きる、それが男！\n〜キャラクターボーナス〜\nストーリークリアポイント✕10ポイント加算");
-                    break;
+                case 0:character = Sprite::create("character_kiki.png");break;
+                case 1:character = Sprite::create("character_petitKiki.png");break;
+                case 2:character = Sprite::create("character_harryPotter.png");break;
+                case 3:character = Sprite::create("character_ponyo.png");break;
+                case 4:character = Sprite::create("character_fordAnglia.png");break;
+                case 5:character = Sprite::create("character_porcoRosso.png");break;
                 default:break;
             }
+            
+            //タップ後のボタンの透明度を変更する
+            character->setOpacity(128);
+            
         }else{
             //はてなの画像を呼び出し
             switch (idx) {
-                case 0:
-                    character = Sprite::create("character_kiki.png");break;
-                    //charaNameText -> setVisible(false);
-                    //charaExText -> setVisible(false);
-                    //charaLockText -> setVisible(false);
+                case 0:character = Sprite::create("character_kiki.png");break;
                 case 1:character = Sprite::create("character_petitKiki_locked.png");break;
-                case 2:character = Sprite::create("character_ponyo_locked.png");break;
+                case 2:character = Sprite::create("character_harryPotter_locked.png");break;
+                case 3:character = Sprite::create("character_ponyo_locked.png");break;
                 case 4:character = Sprite::create("character_fordAnglia_locked.png");break;
                 case 5:character = Sprite::create("character_porcoRosso_locked.png");break;
                 default:break;
             }
+            
+            int charaPoint;
+            
+            switch (idx) {
+                case 0:break;
+                case 1:charaPoint = 0;
+                case 2:charaPoint = 200;break;
+                case 3:charaPoint = 500;break;
+                case 4:charaPoint = 3000;break;
+                case 5:charaPoint = 10000;break;
+                default:break;
+            }
+            
+            if (userDef -> getIntegerForKey("playPoint") >= charaPoint) {
+                character ->setOpacity(255);
+            }else{
+                
+                character->setOpacity(128);
+                
+            }
         }
-        
-        //タップ後のボタンの透明度を変更する
-        character->setOpacity(128);
         
         //後で一括して透明度変更する際に配列つかうことにしました
         characters->pushBack(character);
@@ -172,13 +170,10 @@ bool SelectCharacterScene::init()
         }else{
             //はてなの画像を呼び出し
             switch (idx) {
-                case 0:
-                    tappedCharacter = Sprite::create("character_kiki.png");break;
-                    //charaNameText -> setVisible(false);
-                    //charaExText -> setVisible(false);
-                    //charaLockText -> setVisible(false);
+                case 0:tappedCharacter = Sprite::create("character_kiki.png");break;
                 case 1:tappedCharacter = Sprite::create("character_petitKiki_locked.png");break;
-                case 2:tappedCharacter = Sprite::create("character_ponyo_locked.png");break;
+                case 2:tappedCharacter = Sprite::create("character_harryPotter_locked.png");break;
+                case 3:tappedCharacter = Sprite::create("character_ponyo_locked.png");break;
                 case 4:tappedCharacter = Sprite::create("character_fordAnglia_locked.png");break;
                 case 5:tappedCharacter = Sprite::create("character_porcoRosso_locked.png");break;
                 default:break;
@@ -191,11 +186,10 @@ bool SelectCharacterScene::init()
 //メニューボタンを作成していく
         MenuItemSprite *pBtnItem;
         
+        //キャラクターが解放されている時の動作
         if(characterFlag == true){
         //メニューアイテムの作成
             pBtnItem = MenuItemSprite::create(character, tappedCharacter, [&](Ref *ref){
-            
-            
             
                 /**
                  *  pSenderをMenuItemにキャスト
@@ -210,12 +204,29 @@ bool SelectCharacterScene::init()
                 
                 //透明度を一括変更
                 for(auto character : *characters){
-                    character->setOpacity(128);
+                    
+                    if (characterFlag == true) {
+                        character->setOpacity(128);
+                    }
+                    
                 }
+                
                 //対象のボタンの透明度を変更
                 characters->at(button->getTag())->setOpacity(255);
                 
-
+                //int playPoint = userDef->getIntegerForKey("playPoint");
+                
+                
+                //対象の説明テキストをセット
+                switch (userDef -> getIntegerForKey("selectCharacter")) {
+                    case 0:charaExLabel->setString(kikiExText);break;
+                    case 1:charaExLabel->setString(petitKikiExText);break;
+                    case 2:charaExLabel->setString(haryyPotterExText);break;
+                    case 3:charaExLabel->setString(ponyoExText);break;
+                    case 4:charaExLabel->setString(fordAngliaExText);break;
+                    case 5:charaExLabel->setString(porcoRossoExText);break;
+                    default:break;
+                }
             
             });
         
@@ -231,33 +242,133 @@ bool SelectCharacterScene::init()
         
         //メニューの作成　pMenuの中にpBtnItemを入れる
         auto Menu = Menu::create(pBtnItem, NULL);
-        
 
         //ポジションを設定
         switch (idx) {
-            case 0:Menu->setPosition(Vec2(selfFrame.width/10*2,selfFrame.height/10*7));break;
-            case 1:Menu->setPosition(Vec2(selfFrame.width/10*5,selfFrame.height/10*7));break;
-            case 2:Menu->setPosition(Vec2(selfFrame.width/10*8,selfFrame.height/10*7));break;
-            case 3:Menu->setPosition(Vec2(selfFrame.width/10*2,selfFrame.height/10*5));break;
-            case 4:Menu->setPosition(Vec2(selfFrame.width/10*5,selfFrame.height/10*5));break;
-            case 5:Menu->setPosition(Vec2(selfFrame.width/10*8,selfFrame.height/10*5));break;
+            case 0:Menu->setPosition(Vec2(selfFrame.width/10*2,selfFrame.height*7/10));break;
+            case 1:Menu->setPosition(Vec2(selfFrame.width/10*5,selfFrame.height*7/10));break;
+            case 2:Menu->setPosition(Vec2(selfFrame.width/10*8,selfFrame.height*7/10));break;
+            case 3:Menu->setPosition(Vec2(selfFrame.width/10*2,selfFrame.height*0.52));break;
+            case 4:Menu->setPosition(Vec2(selfFrame.width/10*5,selfFrame.height*0.52));break;
+            case 5:Menu->setPosition(Vec2(selfFrame.width/10*8,selfFrame.height*0.52));break;
 
             default:break;
-
-
-        
         }
         
         bg->addChild(Menu);
         
         
+        //キャラクターの価格および名前
+        auto priceAndCharaLabel = Label::createWithSystemFont("", "MagicSchoolOne", 25);
+        priceAndCharaLabel -> setColor(Color3B::BLACK);
+        
+        
+        //キャラネームと価格
+        if (characterFlag == true) {
+            
+            switch (idx) {
+                case 0:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("見習い魔女");
+                    break;
+                case 1:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("ひよっこ魔女");
+                    break;
+                case 2:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("☇魔法使い☇");
+                    break;
+                case 3:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("まっかの女の子");
+                    break;
+                case 4:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("空飛ぶ車");
+                    break;
+                case 5:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("元ｴｰｽ･ﾊﾟｲﾛｯﾄ");
+                    break;
+                default:break;
+            }
+            
+        }else{
+            
+            switch (idx) {
+                case 0:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("見習い魔女");
+                    break;
+                case 1:
+                    priceAndCharaLabel -> setSystemFontName("HiraKakuPro-W3");
+                    priceAndCharaLabel -> setString("レビューでゲット!");
+                    priceAndCharaLabel -> setColor(Color3B::RED);
+                    priceAndCharaLabel -> setSystemFontSize(20);
+                    break;
+                case 2:
+                    priceAndCharaLabel -> setSystemFontName("MagicSchoolOne");
+                    priceAndCharaLabel -> setString("200PT");
+                    priceAndCharaLabel -> setSystemFontSize(40);
+                    break;
+                case 3:
+                    priceAndCharaLabel -> setSystemFontName("MagicSchoolOne");
+                    priceAndCharaLabel -> setString("500PT");
+                    priceAndCharaLabel -> setSystemFontSize(40);
+                    break;
+                case 4:
+                    priceAndCharaLabel -> setSystemFontName("MagicSchoolOne");
+                    priceAndCharaLabel -> setString("3000Pt");
+                    priceAndCharaLabel -> setSystemFontSize(40);
+                    break;
+                case 5:
+                    priceAndCharaLabel -> setSystemFontName("MagicSchoolOne");
+                    priceAndCharaLabel -> setString("10000Pt");
+                    priceAndCharaLabel -> setSystemFontSize(40);
+                    break;
+                default:break;
+            }
+        
+        }
+        
+        //キャラネームおよび価格のポジション
+        switch (idx) {
+            case 0:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*2,selfFrame.height*0.62));break;
+            case 1:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*5,selfFrame.height*0.62));
+                break;
+            case 2:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*8,selfFrame.height*0.62));
+                break;
+            case 3:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*2,selfFrame.height*0.435));
+                break;
+            case 4:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*5,selfFrame.height*0.435));
+                break;
+            case 5:priceAndCharaLabel -> setPosition(Vec2(selfFrame.width/10*8,selfFrame.height*0.435));
+                break;
+            default:break;
+        }
+        
+        this -> addChild(priceAndCharaLabel);
         
     }
+    //ループ文終了
     
 
     //選択しているキャラのアイコンを黒に(初期位置設定)
     characters->at(userDef->getIntegerForKey("selectCharacter"))->setOpacity(255);
+    
+    switch (userDef->getIntegerForKey("selectCharacter")){
+        case 0:charaExLabel->setString(kikiExText);break;
+        case 1:charaExLabel->setString(petitKikiExText);break;
+        case 2:charaExLabel->setString(haryyPotterExText);break;
+        case 3:charaExLabel->setString(ponyoExText);break;
+        case 4:charaExLabel->setString(fordAngliaExText);break;
+        case 5:charaExLabel->setString(porcoRossoExText);break;
+        default:break;
+            
+    }
 
+    
     
     
     this->setBackBt();
--- Classes/SelectCharacterScene.h
+++ Classes/SelectCharacterScene.h
@@ -26,10 +26,22 @@ public:
     
     //配列(透明度の一括管理のため)
     Vector<Sprite*> *characters = new Vector<Sprite*>;
-    Label *charaNameText;
-    Label *charaExText;
-    Label *charaLockText;
+    Label *charaExLabel;
     
+    //キキ
+    std::string kikiExText = "飛ぶのが得意なおてんば少女\n夢に向かって修行中";
+    
+    //おまけキキ
+    std::string petitKikiExText = "飛ぶのが苦手なあなたに、まずは練習用\n～キャラクターボーナス～\n障害物にあたりにくくなる";
+    //ハリーポッター
+    std::string haryyPotterExText = "僕は生き残る！\n～キャラクターボーナス～\nストーリクリアポイントに10ポイント加算";
+    //ポニョ
+    std::string ponyoExText = "元気いっぱい！空もかけちゃお♪\n～キャラクターボーナス～\nストーリクリアポイント✕3ポイント加算";
+    //空飛ぶ車
+    std::string fordAngliaExText = "オジサンからかりた車\n～キャラクターボーナス～\nストーリークリアポイント✕5ポイント加算";
+    //ポルコ・ロッソ
+    std::string porcoRossoExText = "自由気ままに生きる、それが男！\n～キャラクターボーナス～\nストーリークリアポイント✕10ポイント加算";
+
 
 };
 
